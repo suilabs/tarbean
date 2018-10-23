a465569e356df6d9eebc4c9f2822a9e4520c43c0
Borja Arias <borja.arias.upc@gmail.com>
2018-10-23 20:27:00 +0000

    Merged in S3_upload (pull request #8)
    Endpoint for S3

---
+++ src/S3Service.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const path = require('path');
+const aws = require('aws-sdk');
+
+const fileParams = (filename, contentType) => ({
+  filename: crypto.randomBytes(16).toString('hex') + path.extname(filename),
+  contentType: contentType || '',
+});
+
+class S3Service {
+  constructor(config, secrets) {
+    this.region = config.S3_REGION;
+    this.bucket = config.S3_BUCKET;
+    this.accessKey = secrets.s3AccessKey;
+    this.secretKey = secrets.s3SecretKey;
+  }
+
+  uploadFile(filename, base64Data, contentType) {
+    return new Promise((resolve, reject) => {
+      const s3 = new aws.S3({
+        apiVersion: '2006-03-01',
+        region: this.region,
+        accessKeyId: this.accessKey,
+        secretAccessKey: this.secretKey,
+      });
+
+      const file = fileParams(filename, contentType);
+
+      const params = {
+        Bucket: this.bucket,
+        Key: file.filename,
+        Body: base64Data,
+        ACL: 'public-read',
+        ContentType: file.contentType,
+      };
+
+      s3.upload(params, (error, data) => {
+        if (error) reject(error);
+        else resolve(data);
+      });
+    });
+  }
+}
+
+module.exports = S3Service;
