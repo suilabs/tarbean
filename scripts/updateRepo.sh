4f50d9f2a7be39e5e5aa772600be1ad320d4f94c
Borja Arias <borja.arias.upc@gmail.com>
2019-06-11 19:39:19 +0000

    Merged in SOFT-96_CircleCIMigration (pull request #15)
    SOFT-96 Add circle ci config and remove bitbucket pipeline

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-ssh suilabs@suilabs.com <<< "
-cd Projects/tarbean
-git pull origin master && \
-docker-compose up --build server"
+LC_PROJECT_NAME=tarbean
+ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${LC_PROJECT_NAME}; exit" && \
+rsync -rpulz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${LC_PROJECT_NAME}/. && \
+ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
+cd Projects/${LC_PROJECT_NAME}
+docker-compose up -d --build;
+exit"
