acabf94a1d9ce80031f000283e07c642b0de7d33
ShakMR <borja.arias.upc@gmail.com>
2021-03-09 21:54:32 +0100

    Update deploy script

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 LC_PROJECT_NAME=tarbean
-ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "mkdir -p Projects/${LC_PROJECT_NAME}; exit" && \
+ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com "mkdir -p Projects/${LC_PROJECT_NAME}" && \
 rsync -rpulz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${LC_PROJECT_NAME}/. && \
-ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com <<< "
-cd Projects/${LC_PROJECT_NAME}
-docker-compose up -d --build;
-exit"
+ssh -tt -o SendEnv=PROJECT_NAME -o StrictHostKeyChecking=no suilabs@suilabs.com "cd Projects/${LC_PROJECT_NAME}; docker-compose up -d --build;"
