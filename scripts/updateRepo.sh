a9402b151a753524380294b9897669e8745a5a01
ShakMR <borja.arias.upc@gmail.com>
2022-01-12 19:39:16 +0100

    Fix deployment script

--- scripts/updateRepo.sh
+++ scripts/updateRepo.sh
@@ -3,4 +3,4 @@
 LC_PROJECT_NAME=tarbean
 ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com "mkdir -p Projects/${LC_PROJECT_NAME}" && \
 rsync -rpulz --verbose ${PWD}/../* suilabs@suilabs.com:./Projects/${LC_PROJECT_NAME}/. && \
-ssh -tt -o SendEnv=LC_* -o SendEnv -o StrictHostKeyChecking=no suilabs@suilabs.com "cd Projects/${LC_PROJECT_NAME}; docker-compose up -d --build;"
+ssh -tt -o StrictHostKeyChecking=no suilabs@suilabs.com "cd Projects/${LC_PROJECT_NAME}; docker-compose up -d --build;"
